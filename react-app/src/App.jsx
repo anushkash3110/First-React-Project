51073f0e665ec0a84283d7de60ecd163d927e37f
Anushka Sharma <anush2007ka@gmail.com>
2026-03-29 16:08:46 +0530

    Navbar added along with basic css

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />    
       <div className="content">
         <Home />
 
--- react-app/src/Home.jsx
+++ react-app/src/Home.jsx
@@ -1,8 +1,20 @@
 const Home = () => {
+    
+    const handleClick = (e) => {
+        console.log('Hello, user!', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello, ' + name, e.target);
+    }
+
+
     return (
         <div className="home">
             <h2>Home</h2>
             <p>Welcome to the home page!</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Anushka', e)}>Click me again</button>
         </div>
     );
 }
