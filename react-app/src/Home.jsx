293f8e0eb525befa86fbff74a5d5b14cb49a579f
Anushka Sharma <anush2007ka@gmail.com>
2026-04-04 16:55:13 +0530

    useEffect added in home page

--- react-app/src/Home.jsx
+++ react-app/src/Home.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -8,15 +9,23 @@ const Home = () => {
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'maria', id: 3}
     ]);
 
+    const [name, setName] = useState('maria');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setblogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>{ blog.body }</p>
-                    <p>- { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title = "All Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName('juliet')}>change name</button>
+            <p>{ name }</p>
 
         </div>
     );
