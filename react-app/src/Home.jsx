0401417c1af7ae1ed7cb027a3360148846a432f7
Anushka Sharma <anush2007ka@gmail.com>
2026-04-05 16:52:58 +0530

    remove option added

--- react-app/src/Home.jsx
+++ react-app/src/Home.jsx
@@ -1,14 +1,23 @@
+
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
     const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     
+    const handleDelete = (id) => {
+    fetch('http://localhost:8000/blogs/' + id, {
+      method: 'DELETE'
+    }).then(() => {
+      window.location.reload(); // simple for now
+    });
+  };
+
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}    
-            {blogs && <BlogList blogs={blogs} title = "All Blogs!"/>} 
+            {blogs && <BlogList blogs={blogs} title = "All Blogs!" handleDelete={handleDelete}/>} 
         </div>
     );
 }
